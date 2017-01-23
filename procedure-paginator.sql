857913f187d93a3a7496afcbd7c427c00ddb9e2d
Victor Santos <victorluissantos@live.com>
2017-01-23 21:24:08 -0200

    Update procedure-paginator.sql

--- procedure-paginator.sql
+++ procedure-paginator.sql
@@ -1,15 +1,22 @@
 DELIMITER //
 CREATE PROCEDURE prc_pearpage(tabela VARCHAR(12), coluna VARCHAR(120), ordem CHAR(4), porpagina INT, pagina INT)
 BEGIN
-    DECLARE de INT;
-    DECLARE ate INT;
-    SET ate = porpagina * pagina;
-    SET de = (ate - pagina)+1;
+	DECLARE valid VARCHAR(120);
+	DECLARE de INT;
+	DECLARE ate INT;
+	DECLARE colunas VARCHAR(255);
+	SET ate = porpagina * pagina;
+	SET de = (ate - pagina)+1;
 
-	SET @sqlStatement = concat("SELECT * FROM ",tabela," ORDER BY ",coluna," ",ordem," LIMIT ",de,",",ate);
-	PREPARE stmt FROM @sqlStatement;
-	EXECUTE stmt;
-END; //
+    CASE
+	WHEN upper(ordem) not in ('ASC','DESC') THEN
+		SELECT ("O Parametro [ordem] esta incorreto, possíveis [ASC, DESC]")
+	ELSE
+		SET @sqlStatement = concat("SELECT * FROM ",tabela," ORDER BY ",coluna," ",ordem," LIMIT ",de,",",ate);
+		PREPARE stmt FROM @sqlStatement;
+		EXECUTE stmt;
+	END CASE;
+END
 DELIMITER ;
 
 /*
