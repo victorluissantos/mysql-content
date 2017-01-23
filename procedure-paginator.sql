bb25c412ad7431dd5a67b7f4a2f2c1b2e2b65e92
Victor Santos <victorluissantos@live.com>
2017-01-23 21:32:50 -0200

    Update procedure-paginator.sql

--- procedure-paginator.sql
+++ procedure-paginator.sql
@@ -1,22 +1,23 @@
+DROP procedure IF EXISTS `prc_pearpage`;
 DELIMITER //
-CREATE PROCEDURE prc_pearpage(tabela VARCHAR(12), coluna VARCHAR(120), ordem CHAR(4), porpagina INT, pagina INT)
+CREATE PROCEDURE `prc_pearpage`(tabela VARCHAR(12), coluna VARCHAR(120), ordem CHAR(4), porpagina INT, pagina INT)
 BEGIN
 	DECLARE valid VARCHAR(120);
-	DECLARE de INT;
-	DECLARE ate INT;
-	DECLARE colunas VARCHAR(255);
-	SET ate = porpagina * pagina;
-	SET de = (ate - pagina)+1;
-
+    DECLARE de INT;
+    DECLARE ate INT;
+    DECLARE colunas VARCHAR(255);
+    SET ate = porpagina * pagina;
+    SET de = (ate - pagina)+1;
+    
     CASE
-	WHEN upper(ordem) not in ('ASC','DESC') THEN
-		SELECT ("O Parametro [ordem] esta incorreto, possíveis [ASC, DESC]")
-	ELSE
-		SET @sqlStatement = concat("SELECT * FROM ",tabela," ORDER BY ",coluna," ",ordem," LIMIT ",de,",",ate);
-		PREPARE stmt FROM @sqlStatement;
-		EXECUTE stmt;
-	END CASE;
-END
+		WHEN upper(ordem) not in ('ASC','DESC') THEN
+			SELECT ("O Parametro [ordem] esta incorreto, possíveis [ASC, DESC]");
+		ELSE
+			SET @sqlStatement = concat("SELECT * FROM ",tabela," ORDER BY ",coluna," ",ordem," LIMIT ",de,",",ate);
+			PREPARE stmt FROM @sqlStatement;
+			EXECUTE stmt;
+		END CASE;
+END//
 DELIMITER ;
 
 /*
